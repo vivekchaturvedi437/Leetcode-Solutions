569ee9b7ad53b89774c0f3590b3ae61519399a18
vivekchaturvedi437 <chaturvedivivek437@gmail.com>
2026-07-13 00:59:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (60.65%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -3,16 +3,15 @@ class Solution {
         
         int zero=0, one=0, two=0;
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 0) zero++;
-            else if(nums[i] == 1) one++;
+        for(int num : nums){
+            if(num == 0) zero++;
+            else if(num == 1) one++;
             else two++;
         }
 
-        for(int i=0; i<nums.length; i++){
-            if(i < zero) nums[i] = 0;
-            else if(i >= zero && i < zero+one) nums[i] = 1;
-            else nums[i] = 2;
-        }
+        int idx = 0;
+        while(zero-- > 0) nums[idx++] = 0;
+        while(one-- > 0) nums[idx++] = 1;
+        while(two-- > 0) nums[idx++] = 2; 
     }
 }
